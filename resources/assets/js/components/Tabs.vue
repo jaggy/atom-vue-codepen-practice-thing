bf00d289e79cdf43874400c6eaf1055ab93d5923
Jaggy Gauran <i.am@jag.gy>
2016-07-05 16:17:23 +0800

    Fix the layout and animation for the tab close icon.

--- resources/assets/js/components/Tabs.vue
+++ resources/assets/js/components/Tabs.vue
@@ -33,16 +33,16 @@ export default {
     <ul class="[ u-lis:n u-d:fx u-h:100p ]">
         <li
             :class="{ 'tab--active': file.id == active.id }"
-            class="tab-bar__tab tab [ u-d:fx u-ai:c u-jc:c u-p:r u-fz:0.8125r u-p:r ]"
+            class="tab-bar__tab tab [ u-d:fx u-p:r u-fz:0.8125r u-p:r ]"
             v-for="file in files">
             <a href="#"
-               class="tab__item [ u-d:b u-w:12r u-pl:0.75r u-pr:0.75r u-c:$tab-bar-font-color u-td:n ]"
+               class="tab__item [ u-d:b u-w:12r u-pl:0.75r u-pr:0.75r u-c:$tab-bar-font-color u-td:n u-h:100p u-d:fx u-ai:c ]"
                @click.prevent="select(file)">
                @{ file.basename }
-           </a>
+            </a>
 
            <a href="#"
-              class="tab__close [ u-c:$tab-bar-font-color u-td:n ]"
+              class="tab__close [ u-c:$tab-bar-font-color u-td:n u-p:a u-r:1r u-t:9 u-f:b ]"
               @click.prevent="close(file)">
               &times;
            </a>
